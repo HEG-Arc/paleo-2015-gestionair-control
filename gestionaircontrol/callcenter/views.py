da05344bc49b717495529c91c105de117fe9022a
bvuille <b.vuille@gmail.com>
2015-05-17 20:23:09 +0200

    ajout des trois méthodes start, stop, beat ainsi que de leurs urls

--- gestionaircontrol/callcenter/views.py
+++ gestionaircontrol/callcenter/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import _thread
+import time
 
 # Create your views here.
+
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    text = """<h1>Bienvenue sur le Call Center !</h1>
+              <p>Application pour le Paléo !</p>"""
+    return HttpResponse(text)
+
+def start(request):
+    """ Déclenche le timer """
+    _thread.start()
+
+def beat(request):
+    """ Affiche le timer """
+    time = 240
+    while time > 0:
+         print (time)
+         time = time-1
+         time.sleep(1)
+
+def stop(request):
+    """ Déclenche le timer """
+    _thread.stop()
