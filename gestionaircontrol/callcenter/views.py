3d3b4ff88e0d27b3c0f06fe5a81ec835da0f43d4
bvuille <b.vuille@gmail.com>
2015-05-26 08:48:48 +0200

    modification des fichiers urls.py, views.py

--- gestionaircontrol/callcenter/views.py
+++ gestionaircontrol/callcenter/views.py
@@ -26,3 +26,36 @@ def beat(request):
 def stop(request):
     """ Déclenche le timer """
     _thread.stop()
+
+def countdown(request):
+    """ Déclenche le timer """
+
+def print(request):
+    return HttpResponse(beat())
+
+
+class Worker(Thread):
+    def run(self):
+        for x in range(240,0):
+            print(x)
+            time.sleep(1)
+
+
+def start():
+    Worker().start()
+
+
+def sleep():
+    time.sleep(1)
+
+
+def date1(request):
+    return render(request, 'date.html', {'date': datetime.now()})
+
+def son(beat):
+    if beat == 240:
+        introson.start()
+    if beat == 225:
+        simulson.start()
+    if beat == 15:
+        finalson.start()
