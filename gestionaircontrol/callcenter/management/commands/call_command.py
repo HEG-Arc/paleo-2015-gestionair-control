d8199f461c27a1b77ae131e0718d24482aee113c
antoinerosselet <antoine.rosselet@he-arc.ch>
2015-07-10 11:14:22 +0200

    call_command updated

--- gestionaircontrol/callcenter/management/commands/call_command.py
+++ gestionaircontrol/callcenter/management/commands/call_command.py
@@ -11,17 +11,10 @@ game_running = True
 class Command(DaemonCommand):
     help = 'Manages the calls'
 
-    def loop_callback(self, num_players):
+    def loop_callback(self):
         """ When a new game starts """
-        num_players = num_players
-        game_running = True
-
-    def exit_callback(self):
-        """ When a game ends """
-        game_running = False
-
-    def handle_noargs(self, **options):
-        """ Make calls on the open channels """
+        #num_players = num_players
+        #game_running = True
 
         # get the list of endpoints in state 'online'
         online_endpoints = asterisk_ari.get_online_endpoints()
@@ -36,9 +29,13 @@ class Command(DaemonCommand):
             channels.append(phone)
             print "Online endpoints: %s" % online_endpoints
             print "Channels ringing: %s" % channels
-            type = 'public'
-            create_call_file(phone.json.get('resource'), type)
+            create_call_file(phone.json.get('resource'))
             del online_endpoints[:]
             online_endpoints = asterisk_ari.get_online_endpoints()
             online_endpoints.remove(phone)
             channels.remove(phone)
+
+    def exit_callback(self):
+        """ When a game ends """
+        game_running = False
+
