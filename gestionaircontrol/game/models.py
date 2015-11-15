82fdb4355dc023d86ef04fab39b92e09b46ba6f1
Boris Fritscher <boris@fritscher.ch>
2015-11-15 17:56:12 +0100

    basic key value config object

--- gestionaircontrol/game/models.py
+++ gestionaircontrol/game/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# TODO create a migration which loads inital data
+class Config(models.Model):
+    key = models.CharField(verbose_name=_("Config key"), max_length=250, blank=False, null=False, primary_key=True)
+    value = models.TextField(verbose_name=_("value of this option"), blank=True, null=True)
+
+    def __unicode__(self):
+        return str(self.key)
