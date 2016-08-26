52802d9a3a385dca53af632675695905a9d7b390
CGA <cedric@gaspoz-fleiner.com>
2016-08-26 16:04:22 +0200

    Updated 3 channels

--- daemons/dmx_daemon.py
+++ daemons/dmx_daemon.py
@@ -32,7 +32,7 @@ COM_PORT = '/dev/ttyUSB0'
 
 mydmx = pysimpledmx.DMXConnection(COM_PORT)
 
-EFFECTS = {'strobe': 101}
+EFFECTS = {'bumper': 300, 'wheel': 310, 'borne': 320}
 PHONES = {'1001': 1, '1002': 5, '1003': 9, '1004': 13, '1005': 17, '1006': 21, '1007': 25, '1008': 29, '1009': 33, '1010': 37}
 
 print "DMX Controller Starting..."
@@ -43,54 +43,80 @@ def send_dmx_scene(scene):
             mydmx.setChannel(*channel)
         mydmx.render()
 
+def set_effect_color(scene, channel, d1, d2, d3, d4, d5, d6, d7):
+    scene.append((channel + 0, d1))
+    scene.append((channel + 1, d2))
+    scene.append((channel + 2, d3))
+    scene.append((channel + 3, d4))
+    scene.append((channel + 4, d5))
+    scene.append((channel + 5, d6))
+    scene.append((channel + 6, d7))
+
 def set_phone_color(scene, channel, r, g, b, w):
     scene.append((channel + 1, r))
     scene.append((channel + 0, g))
     scene.append((channel + 2, b))
     scene.append((channel + 3, w))
 
-def powerdown_scene():
+def default_scene():
     scene = []
-    # Strobe
-    scene.append((EFFECTS['strobe'], 255))  # CH1 intensité
-    scene.append((EFFECTS['strobe'] + 1, 255))  # CH2 vitesse
-    scene.append((EFFECTS['strobe'] + 6, 40))  # CH7 mode (35-69 zone 1-3)
-    # Tous téléphones bleu
-    for number, channel in PHONES.iteritems():
-        set_phone_color(scene, channel, 0, 0, 255, 0)
-    send_dmx_scene(scene)
-    scene = []
-    # Attendre 2 secondes
-    time.sleep(2)
-    # Stop le strobo
-    scene.append((EFFECTS['strobe'], 0))  # CH1 intensité
-    scene.append((EFFECTS['strobe'] + 6, 40))  # CH7 mode (35-69 zone 1-3)
-    send_dmx_scene(scene)
-    scene = []
-    # Fade out
-    for i in range(1, 5):
-        for number, channel in PHONES.iteritems():
-            set_phone_color(scene, channel, 0, 0, 250 - i * 50, 0)
-        send_dmx_scene(scene)
-        scene = []
-        time.sleep(1)
-    # Black
-    for number, channel in PHONES.iteritems():
-        set_phone_color(scene, channel, 0, 0, 0, 0)
+    set_effect_color(scene, EFFECTS['wheel'], 0, 138, 201, 0, 0, 0, 100)
+    set_effect_color(scene, EFFECTS['bumper'], 0, 138, 201, 0, 0, 0, 255)
     send_dmx_scene(scene)
 
-def start_wheel():
-    scene = []
-    # Strobe
-    scene.append((300, 255))  # CH1 intensité
-    scene.append((301, 255))  # CH1 intensité
-    scene.append((302, 255))  # CH1 intensité
-    scene.append((303, 255))  # CH1 intensité
+def wheel_small(scene):
+    set_effect_color(scene, EFFECTS['wheel'], 0, 0, 0, 0, 240, 180, 255)
     send_dmx_scene(scene)
+    time.sleep(3)
+    default_scene()
+
+def wheel_big(scene):
+    set_effect_color(scene, EFFECTS['wheel'], 0, 0, 0, 0, 240, 180, 255)
+    send_dmx_scene(scene)
+    time.sleep(5)
+    default_scene()
+
+def start_wheel(size):
+    scene = []
+    scene.append((EFFECTS['wheel'] + 0,   0))  # CH1
+    scene.append((EFFECTS['wheel'] + 1, 138))  # CH2
+    scene.append((EFFECTS['wheel'] + 2, 201))  # CH3
+    scene.append((EFFECTS['wheel'] + 3,   0))  # CH4
+    scene.append((EFFECTS['wheel'] + 4, 220))  # CH5
+    scene.append((EFFECTS['wheel'] + 5,   0))  # CH6
+    scene.append((EFFECTS['wheel'] + 6, 255))  # CH7
+    send_dmx_scene(scene)
+    time.sleep(4)
+    scene = []
+    scene.append((EFFECTS['wheel'] + 0,   0))  # CH1
+    scene.append((EFFECTS['wheel'] + 1, 138))  # CH2
+    scene.append((EFFECTS['wheel'] + 2, 201))  # CH3
+    scene.append((EFFECTS['wheel'] + 3,   0))  # CH4
+    scene.append((EFFECTS['wheel'] + 4, 100))  # CH5
+    scene.append((EFFECTS['wheel'] + 5,   0))  # CH6
+    scene.append((EFFECTS['wheel'] + 6, 255))  # CH7
+    send_dmx_scene(scene)
+    time.sleep(4)
+    scene = []
+    scene.append((EFFECTS['wheel'] + 0,   0))  # CH1
+    scene.append((EFFECTS['wheel'] + 1, 138))  # CH2
+    scene.append((EFFECTS['wheel'] + 2, 201))  # CH3
+    scene.append((EFFECTS['wheel'] + 3,   0))  # CH4
+    scene.append((EFFECTS['wheel'] + 4,  16))  # CH5
+    scene.append((EFFECTS['wheel'] + 5,   0))  # CH6
+    scene.append((EFFECTS['wheel'] + 6, 255))  # CH7
+    send_dmx_scene(scene)
+    time.sleep(3)
+    if size == 'big':
+       wheel_big(scene)
+    elif size == 'small':
+       wheel_small(scene)
 
 def stop_scene(scene):
     for number, channel in PHONES.iteritems():
         set_phone_color(scene, channel, 0, 0, 0, 0)
+    for number, channel in EFFECTS.iteritems():
+        set_effect_color(scene, channel, 0, 0, 0, 0, 0, 0, 0)
 
 
 def play_dmx_from_event(event):
@@ -98,14 +124,14 @@ def play_dmx_from_event(event):
 
     if event['type'] == 'GAME_START':
         for number, channel in PHONES.iteritems():
-            set_phone_color(scene, channel, 0, 0, 255, 0)
+            set_phone_color(scene, channel, 0, 138, 201, 0)
     if event['type'] == 'GAME_START_PLAYING':
         for number, channel in PHONES.iteritems():
             set_phone_color(scene, channel, 0, 0, 0, 0)
     elif event['type'] == 'PHONE_RINGING':
         number = event['number']
         channel = PHONES['%s' % number]
-        set_phone_color(scene, channel, 0, 0, 255, 0)
+        set_phone_color(scene, channel, 0, 138, 201, 0)
     elif event['type'] == 'PHONE_STOPRINGING':
         number = event['number']
         channel = PHONES['%s' % number]
@@ -126,7 +152,14 @@ def play_dmx_from_event(event):
         # TODO: Détacher le proc
         powerdown_scene()
     elif event['type'] == 'WHEEL_START':
-        start_wheel()
+        start_wheel(event['size'])
+    elif event['type'] == 'PLAYER_SCANNED':
+        if event['state'] == 'SCANNED_WHEEL':
+            set_effect_color(scene, EFFECTS['bumper'], 0, 255, 0, 0, 0, 0, 255)
+        elif event['state'] == 'SCANNED_PEN':
+            set_effect_color(scene, EFFECTS['bumper'], 0, 138, 201, 0, 0, 0, 255)
+        else:
+            set_effect_color(scene, EFFECTS['bumper'], 255, 0, 0, 0, 0, 0, 255)
     elif event['type'] == 'STOP':
         stop_scene(scene)
 
